cb8135211d6e4d29a5c5de5855dee20c458b3482
kaz <kazuki.amatatsu.fd@gmail.com>
2023-08-06 17:50:14 +0900

    feat: リアクションがあったメッセージを取得する

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
+require("dotenv").config();
+
 const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
-  // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
-  // 何らかのポートをリッスンする必要があります
-  port: process.env.PORT || 3000,
 });
 
-// "hello" を含むメッセージをリッスンします
-app.message("hello", async ({ message, say }) => {
-  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
-  await say({
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `Hey there <@${message.user}>!`,
-        },
-        accessory: {
-          type: "button",
-          text: {
-            type: "plain_text",
-            text: "Click Me",
-          },
-          action_id: "button_click",
-        },
-      },
-    ],
-    text: `Hey there <@${message.user}>!`,
-  });
-});
+let cursor;
+async function findConversation() {
+  try {
+    const channelList = await app.client.conversations.list({
+      token: process.env.SLACK_USER_TOKEN,
+      exclude_archived: true,
+      limit: 1000,
+      cursor,
+    });
+    if (channelList.response_metadata.next_cursor) {
+      cursor = channelList.response_metadata.next_cursor;
+      findConversation();
+    }
 
-app.action("button_click", async ({ body, ack, say }) => {
-  // アクションのリクエストを確認
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
+    channelList.channels.forEach(async (channel) => {
+      if (channel.name === "slack_api_test") {
+        const conversation = await app.client.conversations.history({
+          token: process.env.SLACK_USER_TOKEN,
+          channel: channel.id,
+          limit: 1000,
+        });
+        // リアクションがついているメッセージを抽出
+        const reactions = conversation.messages.filter((message) => {
+          return message.reactions;
+        });
+
+        // パーマリンクを取得
+        reactions.forEach(async (reaction) => {
+          const permalink = await app.client.chat.getPermalink({
+            token: process.env.SLACK_USER_TOKEN,
+            channel: channel.id,
+            message_ts: reaction.ts,
+          });
+          console.log("permalink:", permalink);
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+findConversation();
 
 (async () => {
   // アプリを起動します
